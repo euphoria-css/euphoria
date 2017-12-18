cb261116dfc8d0a6260e32e6d1aafc0c9dffb87f
Dana Woodman <dana@danawoodman.com>
2017-12-18 13:12:30 -1000

    Remove v1 code

--- euphoria/floats.js
+++
@@ -1,9 +0,0 @@
-const rule = require('./utils/create-rule')
-
-module.exports = () => {
-  return [
-    rule('fl', ['float', 'left']),
-    rule('fr', ['float', 'right']),
-    rule('fn', ['float', 'none']),
-  ]
-}
--- euphoria/utils/create-rule.js
+++
@@ -1,9 +0,0 @@
-/**
- * Create a CSS rule given a name and a list of rules.
- * 
- * createRule('my-rule', ['display', 'block'], ['color', 'red'])
- */
-module.exports = (name, ...rules) => {
-  const all = rules.map(r => `${r[0]}: ${r[1]} !important;`).join(' ')
-  return `.${name} { ${all} }`
-}
