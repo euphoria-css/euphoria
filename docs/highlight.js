83e64fcc66d9a3f0c7c0dcf7229ccda095a6724c
Dana Woodman <dana@danawoodman.com>
2017-12-29 13:08:26 -1000

    Add HTML to highlight

--- docs/highlight.js
+++ docs/highlight.js
@@ -1,18 +1,22 @@
+import bash from 'react-syntax-highlighter/dist/languages/bash'
 import css from 'react-syntax-highlighter/dist/languages/css'
-import vs from 'react-syntax-highlighter/dist/styles/vs'
 import js from 'react-syntax-highlighter/dist/languages/javascript'
 import PropTypes from 'prop-types'
 import React from 'react'
 import SyntaxHighlighter, {
   registerLanguage,
 } from 'react-syntax-highlighter/dist/light'
+import vs from 'react-syntax-highlighter/dist/styles/vs'
+import xml from 'react-syntax-highlighter/dist/languages/xml'
 
 registerLanguage('javascript', js)
 registerLanguage('css', css)
+registerLanguage('html', xml)
+registerLanguage('bash', bash)
 
 Highlight.propTypes = {
   children: PropTypes.string.isRequired,
-  lang: PropTypes.oneOf(['javascript', 'css']).isRequired,
+  lang: PropTypes.oneOf(['javascript', 'css', 'html']).isRequired,
 }
 
 function Highlight({ children, lang }) {
