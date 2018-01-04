5e5837f6bc3fea1968de3ea0116fc723122de512
Dana Woodman <dana@danawoodman.com>
2018-01-04 10:42:03 -1000

    Remove old docs and src folders, fix build scripts.

--- docs/ruleset-anchor.js
+++
@@ -1,7 +0,0 @@
-import slugify from 'url-slug'
-
-function ruleSetAnchor(name = '') {
-  return slugify(name.toLowerCase())
-}
-
-export default ruleSetAnchor
--- docs/stringify.js
+++
@@ -1,5 +0,0 @@
-import stringifyObject from 'stringify-object'
-
-export default function stringify(code) {
-  return stringifyObject(code, { indent: '  ' })
-}
